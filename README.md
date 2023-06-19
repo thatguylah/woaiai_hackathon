cb2c991d77c2592015c1935fe5918943c186142c
Yao Kuan <chan_yao_kuan@tech.gov.sg>
2023-06-19 23:56:03 +0800

    minimum viable product!!! hugging face works

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # woaiai_hackathon
 dsaid hackathon 2023 
+
+## Description
+- This folder holds the python code for the telegram bot to be run on the server side. (TODO: Convert to serverless instead of bare metal ec2?)
+- .env file contains important tokens to be stored in secrets manager (TODO)
+- bot.py scripts saves file to local working directory, overwriting the latest file each time. 
+
+## How to run locally 
+- cd woaiai_hackathon
+- pip install -r requirements.txt
+- python3 bot.py
+
+## How to deploy to AWS
+- For now, we will deploy to an internet VPC on bare metal EC2, that EC2 instance will point to github to pull the code to run on the server
--- bot.py
+++ bot.py
@@ -1,11 +1,105 @@
-import telebot
+# pylint: disable=unused-argument, wrong-import-position
+# This program is dedicated to the public domain under the CC0 license.
 
+"""
+Simple Bot to reply to Telegram messages.
+
+First, a few handler functions are defined. Then, those functions are passed to
+the Application and registered at their respective places.
+Then, the bot is started and runs until we press Ctrl-C on the command line.
+
+Usage:
+Basic Echobot example, repeats messages.
+Press Ctrl-C on the command line or send a signal to the process to stop the
+bot.
+"""
+
+import logging
+import json
 from dotenv import dotenv_values
-
 import requests
+import json
+from huggingface_hub import InferenceClient
+# from flask import Flask
+# from flask import request
+# from flask import Response
 
 config = dotenv_values(".env")
+TELEBOT_TOKEN = config['TELEBOT_TOKEN']
+HF_TOKEN = config['HF_API_KEY']
 
-BOT_TOKEN = config['BOT_TOKEN']
+from telegram import __version__ as TG_VER
 
-bot = telebot.TeleBot(BOT_TOKEN)
+try:
+    from telegram import __version_info__
+except ImportError:
+    __version_info__ = (0, 0, 0, 0, 0)  # type: ignore[assignment]
+
+if __version_info__ < (20, 0, 0, "alpha", 1):
+    raise RuntimeError(
+        f"This example is not compatible with your current PTB version {TG_VER}. To view the "
+        f"{TG_VER} version of this example, "
+        f"visit https://docs.python-telegram-bot.org/en/v{TG_VER}/examples.html"
+    )
+from telegram import ForceReply, Update
+from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
+
+# Enable logging
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
+logger = logging.getLogger(__name__)
+
+# Set the API endpoint and your API key
+# api_endpoint = "https://api.openai.com/v1/completions"
+# api_key = config['OPENAI_API_KEY']
+
+# Set up the model and prompt
+# model_engine = "text-davinci-003"
+
+# Define a few command handlers. These usually take the two arguments update and
+# context.
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Send a message when the command /start is issued."""
+    user = update.effective_user
+    await update.message.reply_html(
+        rf"Hi {user.mention_html()}!",
+        reply_markup=ForceReply(selective=True),
+    )
+
+
+async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Send a message when the command /help is issued."""
+    await update.message.reply_text("Help!")
+
+
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Echo the user message."""
+    client = InferenceClient(token=HF_TOKEN)
+    image = client.text_to_image(update.message.text)
+    image_path="generic_photo.png"
+    image.save(image_path)
+    await context.bot.send_photo(
+        chat_id=update.effective_chat.id,
+        photo=open(image_path, "rb")
+    )
+
+
+def main() -> None:
+    """Start the bot."""
+    # Create the Application and pass it your bot's token.
+    application = Application.builder().token(TELEBOT_TOKEN).build()
+    
+    # on different commands - answer in Telegram
+    application.add_handler(CommandHandler("start", start))
+    application.add_handler(CommandHandler("help", help_command))
+
+    # on non command i.e message - echo the message on Telegram
+    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
+
+    # Run the bot until the user presses Ctrl-C
+    application.run_polling()
+
+
+if __name__ == "__main__":
+    main()
