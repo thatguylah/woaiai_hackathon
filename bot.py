10dbf62b9f50f66c0810c945b9ef08e3709c985f
Yao Kuan <chan_yao_kuan@tech.gov.sg>
2023-06-20 17:06:06 +0800

    added logger to bot and added optional api routing

--- bot.py
+++ bot.py
@@ -13,19 +13,19 @@ Basic Echobot example, repeats messages.
 Press Ctrl-C on the command line or send a signal to the process to stop the
 bot.
 """
-
+import boto3
 import logging
 import json
 from dotenv import dotenv_values
 import requests
 import json
+import argparse
 from huggingface_hub import InferenceClient
 # from flask import Flask
 # from flask import request
 # from flask import Response
 
 config = dotenv_values(".env")
-TELEBOT_TOKEN = config['TELEBOT_TOKEN']
 HF_TOKEN = config['HF_API_KEY']
 
 from telegram import __version__ as TG_VER
@@ -50,13 +50,6 @@ logging.basicConfig(
 )
 logger = logging.getLogger(__name__)
 
-# Set the API endpoint and your API key
-# api_endpoint = "https://api.openai.com/v1/completions"
-# api_key = config['OPENAI_API_KEY']
-
-# Set up the model and prompt
-# model_engine = "text-davinci-003"
-
 # Define a few command handlers. These usually take the two arguments update and
 # context.
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
@@ -70,36 +63,56 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 
 async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Send a message when the command /help is issued."""
+    helpLogger = logging.getLogger("helpProcess")
+    update_as_json = json.dumps(update.to_dict())
+    helpLogger.info(update_as_json)
+    helpLogger.info(update.effective_chat.first_name + " "+ "sent the message of:" + update.message.text)
     await update.message.reply_text("Help!")
-
+    
 
 async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Echo the user message."""
     client = InferenceClient(token=HF_TOKEN)
+    echoLogger = logging.getLogger("echoProcess")
+    update_as_json = json.dumps(update.to_dict())
+    echoLogger.info(update_as_json)
+    echoLogger.info(update.effective_chat.first_name + " "+ "sent the message of:" + update.message.text)
+
     image = client.text_to_image(update.message.text)
     image_path="generic_photo.png"
     image.save(image_path)
+    
     await context.bot.send_photo(
         chat_id=update.effective_chat.id,
-        photo=open(image_path, "rb")
+        photo=open(image_path, "rb"),
+        write_timeout=150,
+        caption=update.message.text
     )
 
 
-def main() -> None:
-    """Start the bot."""
+def main(dev_mode) -> None:
+    # Start the bot.
     # Create the Application and pass it your bot's token.
-    application = Application.builder().token(TELEBOT_TOKEN).build()
-    
-    # on different commands - answer in Telegram
-    application.add_handler(CommandHandler("start", start))
-    application.add_handler(CommandHandler("help", help_command))
+	if dev_mode:
+		TELEBOT_TOKEN = config['TELEBOT_DEV_TOKEN']
+	else:
+		TELEBOT_TOKEN = config['TELEBOT_TOKEN']
 
-    # on non command i.e message - echo the message on Telegram
-    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
+	application = Application.builder().token(TELEBOT_TOKEN).build()
+	# on different commands - answer in Telegram
+	application.add_handler(CommandHandler("start", start))
+	application.add_handler(CommandHandler("help", help_command))
 
-    # Run the bot until the user presses Ctrl-C
-    application.run_polling()
+	# on non command i.e message - echo the message on Telegram
+	application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
+
+	# Run the bot until the user presses Ctrl-C
+	application.run_polling()
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-DEV", "--dev", action="store_true", help="Run with local Tele API token")
+    args = parser.parse_args()
+
+    main(args.dev)
