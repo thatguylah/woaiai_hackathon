7e503abe4f59a34ae74677421a776ae1a4a4ff2e
Yao Kuan <chan_yao_kuan@tech.gov.sg>
2023-06-26 12:22:30 +0800

    implemented botv2

--- bot.py
+++ bot.py
@@ -21,12 +21,13 @@ import requests
 import json
 import argparse
 from huggingface_hub import InferenceClient
+
 # from flask import Flask
 # from flask import request
 # from flask import Response
 
 config = dotenv_values(".env")
-HF_TOKEN = config['HF_API_KEY']
+HF_TOKEN = config["HF_API_KEY"]
 
 from telegram import __version__ as TG_VER
 
@@ -42,16 +43,24 @@ if __version_info__ < (20, 0, 0, "alpha", 1):
         f"visit https://docs.python-telegram-bot.org/en/v{TG_VER}/examples.html"
     )
 from telegram import ForceReply, Update
-from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
+from telegram.ext import (
+    Application,
+    CommandHandler,
+    ContextTypes,
+    MessageHandler,
+    filters,
+)
 
 # Enable logging
 logging.basicConfig(
-    format="%(asctime)s - %(processName)s - %(threadName)s - [%(thread)d] - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+    format="%(asctime)s - %(processName)s - %(threadName)s - [%(thread)d] - %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO,
 )
 logger = logging.getLogger(__name__)
-huggingFaceLogger = logging.getLogger("huggingface_hub").setLevel(logging.DEBUG)
+huggingFaceLogger = logging.getLogger("InferenceClient").setLevel(logging.DEBUG)
 messageHandlerLogger = logging.getLogger("telegram.bot").setLevel(logging.DEBUG)
 applicationLogger = logging.getLogger("telegram.ext").setLevel(logging.DEBUG)
+
 
 # Define a few command handlers. These usually take the two arguments update and
 # context.
@@ -68,53 +77,66 @@ async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> No
     """Send a message when the command /help is issued."""
     update_as_json = json.dumps(update.to_dict())
     logger.info(update_as_json)
-    logger.info(update.effective_chat.first_name + " "+ "sent the message of:" + update.message.text)
+    logger.info(
+        update.effective_chat.first_name
+        + " "
+        + "sent the message of:"
+        + update.message.text
+    )
     await update.message.reply_text("Help!")
-    
+
 
 async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Echo the user message."""
-    client = InferenceClient(token=HF_TOKEN)
+    Model = "prompthero/openjourney-v4"
+    client = InferenceClient(model=Model,token=HF_TOKEN)
     update_as_dict = update.to_dict()
     update_as_json = json.dumps(update_as_dict)
     logger.info(update_as_json)
-    logger.info(update_as_dict["message"]["from"]["first_name"]+ " "+ "sent the message of:" + update.message.text)
+    logger.info(
+        update_as_dict["message"]["from"]["first_name"]
+        + " "
+        + "sent the message of:"
+        + update.message.text
+    )
 
     image = client.text_to_image(update.message.text)
-    image_path="generic_photo.png"
+    image_path = "generic_photo.png"
     image.save(image_path)
-    
+
     await context.bot.send_photo(
         chat_id=update.effective_chat.id,
         photo=open(image_path, "rb"),
         write_timeout=150,
-        caption=update.message.text
+        caption=update.message.text,
     )
 
 
 def main(dev_mode) -> None:
     # Start the bot.
     # Create the Application and pass it your bot's token.
-	if dev_mode:
-		TELEBOT_TOKEN = config['TELEBOT_DEV_TOKEN']
-	else:
-		TELEBOT_TOKEN = config['TELEBOT_TOKEN']
+    if dev_mode:
+        TELEBOT_TOKEN = config["TELEBOT_DEV_TOKEN"]
+    else:
+        TELEBOT_TOKEN = config["TELEBOT_TOKEN"]
 
-	application = Application.builder().token(TELEBOT_TOKEN).build()
-	# on different commands - answer in Telegram
-	application.add_handler(CommandHandler("start", start))
-	application.add_handler(CommandHandler("help", help_command))
+    application = Application.builder().token(TELEBOT_TOKEN).build()
+    # on different commands - answer in Telegram
+    application.add_handler(CommandHandler("start", start))
+    application.add_handler(CommandHandler("help", help_command))
 
-	# on non command i.e message - echo the message on Telegram
-	application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND , echo))
+    # on non command i.e message - echo the message on Telegram
+    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
 
-	# Run the bot until the user presses Ctrl-C
-	application.run_polling()
+    # Run the bot until the user presses Ctrl-C
+    application.run_polling()
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("-DEV", "--dev", action="store_true", help="Run with local Tele API token")
+    parser.add_argument(
+        "-DEV", "--dev", action="store_true", help="Run with local Tele API token"
+    )
     args = parser.parse_args()
 
     main(args.dev)
